1c869fc1f22cd28b1637f5eab09fb1de47f39b75
Daniel Compton <desk@danielcompton.net>
2016-03-08 16:50:50 +1300

    Add a route to test ip addresses

--- src/clojure_getting_started/web.clj
+++ src/clojure_getting_started/web.clj
@@ -4,18 +4,30 @@
             [compojure.route :as route]
             [clojure.java.io :as io]
             [ring.adapter.jetty :as jetty]
-            [environ.core :refer [env]]))
+            [environ.core :refer [env]]
+            [ring.middleware.proxy-headers :as proxy]))
 
 (defn splash []
-  {:status 200
+  {:status  200
    :headers {"Content-Type" "text/plain"}
-   :body "Hello from Heroku"})
+   :body    "Hello from Heroku"})
 
-(defroutes app
+(defn get-ip [req]
+  {:status  200
+   :headers {"Content-Type" "text/plain"}
+   :body    (str {:remote-addr     (:remote-addr req)
+                  :x-forwarded-for (get-in req [:headers "x-forwarded-for"])})})
+
+(defroutes app-routes
   (GET "/" []
-       (splash))
+    (splash))
+  (GET "/ip" [] get-ip)
   (ANY "*" []
-       (route/not-found (slurp (io/resource "404.html")))))
+    (route/not-found (slurp (io/resource "404.html")))))
+
+(def app
+  (-> app-routes
+      (proxy/wrap-forwarded-remote-addr)))
 
 (defn -main [& [port]]
   (let [port (Integer. (or port (env :port) 5000))]
